58264596d9e25333aee749411e8ee90fac8e49fc
Amélie HUSSON <103633+Othella@users.noreply.github.com>
2024-09-26 13:53:33 +0200

    fix: constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,14 @@
 // TODO use .env file
 
+// main component address
 export const componentAddress: string =
-  "component_tdx_2_1cqvvhn6qe4rq7ax5j3jh2x6k9vy5ruyz47clxqklnh68nxe7u2sfq0";
+  "component_tdx_2_1cpvvlc94hhhx4d5m5cpusnqxqdqvygsxec4gfzw7grmdu4uh7sfdsh";
 
+// Solarix dApp definition address
 export const dAppDefinitionAddress: string =
-  "account_tdx_2_12y7ue9sslrkpywpgqyu3nj8cut0uu5arpr7qyalz7y9j7j5q4ayhv6";
+  "account_tdx_2_12ydxzwpcn7l7cpnmgt8hdr576zme0yxld6h9ua2jndjnrtxkud85nr";
 
-export const adminBadgeAddress: string = "resource_tdx_2_1nt2mn0du834e2ufpm0dtjw3tdd0qn3utmeprtntz49a6784jcwn3h5:#1#";
+export const adminBadgeAddress: string = "resource_tdx_2_1ngcchy9ptuzdss6d7djzqq95uxtjhv00q65q4qzaesg907ljljhy24";
 
 export const dbFile: string = './solarix.sqlite';
 
