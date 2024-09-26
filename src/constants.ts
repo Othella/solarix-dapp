476aefcdde4ffef7dd969a96374c63a22cec79e8
Amélie HUSSON <103633+Othella@users.noreply.github.com>
2024-09-26 13:38:12 +0200

    fix: cleaned constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,24 +1,13 @@
 // TODO use .env file
 
-
 export const componentAddress: string =
-  "component_tdx_2_1cz44jlxyv0wtu2cj7vrul0eh8jpcfv3ce6ptsnat5guwrdlhfpyydn";
+  "component_tdx_2_1cqvvhn6qe4rq7ax5j3jh2x6k9vy5ruyz47clxqklnh68nxe7u2sfq0";
 
-// You can create a dApp definition in the dev console at https://stokenet-console.radixdlt.com/dapp-metadata
-// then use that account for your dAppDefinitionAddress
 export const dAppDefinitionAddress: string =
   "account_tdx_2_12y7ue9sslrkpywpgqyu3nj8cut0uu5arpr7qyalz7y9j7j5q4ayhv6";
 
-export const adminAddress: string = "account_tdx_2_12y7ue9sslrkpywpgqyu3nj8cut0uu5arpr7qyalz7y9j7j5q4ayhv6";
-
 export const adminBadgeAddress: string = "resource_tdx_2_1nt2mn0du834e2ufpm0dtjw3tdd0qn3utmeprtntz49a6784jcwn3h5:#1#";
 
-export const instantiatedComponentAddress: string = "component_tdx_2_1cqvvhn6qe4rq7ax5j3jh2x6k9vy5ruyz47clxqklnh68nxe7u2sfq0";
-
-export const userAdresses: string[] = [
-
-]
-
-export const adminNftResourceAddress: string = "resource_tdx_2_12y7ue9sslrkpywpgqyu3nj8cut0uu5arpr7qyalz7y9j7j5q4ayhv6";
-
 export const dbFile: string = './solarix.sqlite';
+
+export const radixApiUrl: string = 'https://stokenet.radixapi.net/';
